7ae72769fede945880b78dbee55c4d555f776117
NicSmi <nicoy.smith@mymona.uwi.edu>
2019-12-12 16:05:53 +0800

    Added Notification API to android app

--- channel_test/android/app/src/main/kotlin/com/example/channel_test/MainActivity.java
+++ channel_test/android/app/src/main/kotlin/com/example/channel_test/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.channel_test;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.os.Build;
 import android.os.Bundle;
 
 import io.flutter.app.FlutterActivity;
@@ -15,15 +19,22 @@ import android.os.BatteryManager;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 public class MainActivity extends FlutterActivity {
     private static final String CHANNEL = "samples.flutter.dev/battery";
+    private static final String N_CHANNEL = "samples.flutter.dev/notification";
+    private static final String CHANNEL_ID = "my.channel.id";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         GeneratedPluginRegistrant.registerWith(this);
+
+        createNotificationChannel();
 
         new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
                 new MethodCallHandler() {
@@ -32,18 +43,72 @@ public class MainActivity extends FlutterActivity {
                         // This method is invoked on the main thread
                         if(call.method.equals("getBatteryLevel")){
                             int batteryLevel = getBatteryLevel();
-                            
+
                             if (batteryLevel != -1){
                                 result.success(batteryLevel);
                             } else {
                                 result.error("UNAVAILABLE", "Battery level not available.", null);
                             }
-                        } else {
+                        }
+                        else {
                             result.notImplemented();
                         }
                     }
                 });
+
+        new MethodChannel(getFlutterView(), N_CHANNEL).setMethodCallHandler(
+                new MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(@NonNull MethodCall methodCall, @NonNull Result result) {
+                        // This method is invoked on the main thread
+                        if(methodCall.method.equals("getNotification")){
+                            showNotification();
+                        }
+                        else {
+                            result.notImplemented();
+                        }
+                    }
+                }
+        );
     }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (VERSION.SDK_INT >= VERSION_CODES.O){
+            CharSequence name = "This Channel";
+            String description = "Description of the channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the the channel with the system; you cant change the importance
+            // or other notification behaviours after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void showNotification(){
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, MainActivity.class);
+        //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle("Nicoy Smith")
+                .setContentText("Nicoy Smith, CEO of NOS Consolidated")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Testing Flutter Channel"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(45, builder.build());
+    }
+
+
 
     private int getBatteryLevel() {
         int batteryLevel = -1;
